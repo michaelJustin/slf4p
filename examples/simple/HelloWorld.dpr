0867196613c774b0a3263fdd5dada244dbc9e633
Michael Justin <michael@mikejustin.com>
2026-04-26 11:29:17 +0200

    Use message argument

--- examples/simple/HelloWorld.dpr
+++ examples/simple/HelloWorld.dpr
@@ -7,7 +7,7 @@ uses
 
 procedure RunDemo;
 begin
-  LOGGER.Debug('Using slf4p ' + SLF4P_VERSION);
+  LOGGER.Debug('Using slf4p %s', [SLF4P_VERSION]);
   LOGGER.Info('Hello, World!');
   LOGGER.Debug('Hit any key');
   ReadLn;
